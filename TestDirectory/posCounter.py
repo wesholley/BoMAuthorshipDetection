e3f65b1de393bab34484d52f3dfadf5686b04958
Christopher Morgan <junk.not@me.com>
2013-11-22 15:17:17 -0700

    Added the granddaddy of scripts. This one will give us our database file...

---
+++ TestDirectory/posCounter.py
@@ -0,0 +1,22 @@
+
+import os
+
+data = {}
+
+
+############################################################
+# This function will open a
+############################################################
+def process_file(fileName):
+	author, block, junk = fileName
+
+	return
+
+############################################################
+# Goal: Nice Data structure to query any pos statistic 
+#		required.
+############################################################
+if __name__ == '__main__':
+	for fileName in os.listdir("."):
+	    if fileName.endswith(".txt"):
+	        print fileName
