cef11cf9f834a30086d39d3432a800bac7b1f910
jeffrpowell <jeff.powell@byu.edu>
2013-11-22 19:28:08 -0700

    next version of interjections script; context checker can check for punctuation and next word matching

--- scripts/interjections.py
+++ scripts/interjections.py
@@ -1,29 +1,55 @@
 import os
 import string
 input_dir = '../200WordBlocks'
-interjections = ['yea', 'o', 'oh', 'wo', 'woe', 'no', 'ah', 'nay', 'how great',
-                 'hallelujah', 'alleluia', 'behold', 'moreover', 'alas', 'amen',
-                 'lo', 'indeed', 'well']
+#these are always interjections in the Book of Mormon
+interjections = ['yea', 'o', 'oh', 'wo', 'woe', 'ah', 'nay', 'hallelujah',
+                 'alleluia' , 'alas', 'amen', 'lo', 'indeed', 'well']
+#these vary, depending on the context
+context_interjections = ['behold', 'no']
+pronouns = ['i', 'we', 'mine', 'there']
 
-def map_interjections(block):
+def check_context(word, word_no_punc, next_word):
+    punctuation = set(string.punctuation)
+    end_char = word[-1:]
+    if end_char in punctuation or next_word in pronouns:
+        return 1
+    return 0
+
+def map_interjections(block, block_no_punc):
     words_found = {}
-    for word in block:
-        word = word.lower()
+    for i in range(len(block)-1):
+        word = block_no_punc[i].lower()
+        can_add_word = 0
         if word in interjections:
+            can_add_word = 1
+        elif word in context_interjections:
+            if i < len(block)-1:
+                can_add_word = check_context(block[i].lower(), word, block[i+1].lower())
+            else:
+                can_add_word = check_context(block[i].lower(), word, block[i].lower())
+
+        if can_add_word == 1:
             if word in words_found:
                 words_found[word] += 1
             else:
                 words_found[word] = 1
+    
     return words_found
 
 def get_block(file, writefile):
     file = open(file, 'r')
     block_200 = str(file.read())
-    exclude = set(string.punctuation)
-    block_200 = ''.join(ch for ch in block_200 if ch not in exclude)
     block_200 = block_200.split()
+    
+    punctuation = set(string.punctuation)
+    #this kills any hyphenated compound words, but there aren't any that
+    #are interjections, so we go ahead with it
+    block_200_no_punc = []
+    for word in block_200:
+        no_punc = ''.join(ch for ch in word if ch not in punctuation)
+        block_200_no_punc.append(no_punc)
     writefile = open(writefile, 'w')
-    writefile.write(repr(map_interjections(block_200)))
+    writefile.write(repr(map_interjections(block_200, block_200_no_punc)))
     writefile.close()
     file.close()
     
