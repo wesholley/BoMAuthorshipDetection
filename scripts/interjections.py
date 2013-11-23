54cee2e71ef1db5ebb4ca5d3ed7c246e497c1e01
jeffrpowell <jeff.powell@byu.edu>
2013-11-23 07:41:56 -0700

    new version of interjections script to better support next-word context; also including generated .inter files in this commit

--- scripts/interjections.py
+++ scripts/interjections.py
@@ -1,18 +1,30 @@
 import os
 import string
 input_dir = '../200WordBlocks'
+
+#FEEL FREE TO ADD TO THESE INTERJECTION LISTS IF YOU CAN FIND OTHER EXAMPLES
+
 #these are always interjections in the Book of Mormon
 interjections = ['yea', 'o', 'oh', 'wo', 'woe', 'ah', 'nay', 'hallelujah',
                  'alleluia' , 'alas', 'amen', 'lo', 'indeed', 'well']
-#these vary, depending on the context
-context_interjections = ['behold', 'no']
-pronouns = ['i', 'we', 'mine', 'there']
+
+#these next two vary, depending on the context
+
+#these need to check whether they are immediately followed by punctuation
+punc_interjections = ['behold', 'no']
+#this dict checks whether the key word is followed by a value word (ignore punc)
+next_word_interjections = {
+    'behold':['i', 'we', 'mine', 'there', 'how']}
 
 def check_context(word, word_no_punc, next_word):
-    punctuation = set(string.punctuation)
-    end_char = word[-1:]
-    if end_char in punctuation or next_word in pronouns:
-        return 1
+    if word_no_punc in punc_interjections:
+        punctuation = set(string.punctuation)
+        end_char = word[-1:]
+        if end_char in punctuation:
+            return 1
+    if word_no_punc in next_word_interjections:
+        if next_word in next_word_interjections[word_no_punc]:
+            return 1
     return 0
 
 def map_interjections(block, block_no_punc):
@@ -22,11 +34,11 @@ def map_interjections(block, block_no_punc):
         can_add_word = 0
         if word in interjections:
             can_add_word = 1
-        elif word in context_interjections:
+        elif word in punc_interjections or word in next_word_interjections:
             if i < len(block)-1:
-                can_add_word = check_context(block[i].lower(), word, block[i+1].lower())
+                can_add_word = check_context(block[i].lower(), word, block_no_punc[i+1].lower())
             else:
-                can_add_word = check_context(block[i].lower(), word, block[i].lower())
+                can_add_word = check_context(block[i].lower(), word, block_no_punc[i].lower())
 
         if can_add_word == 1:
             if word in words_found:
@@ -51,6 +63,7 @@ def get_block(file, writefile):
     writefile = open(writefile, 'w')
     writefile.write(repr(map_interjections(block_200, block_200_no_punc)))
     writefile.close()
+    map_interjections(block_200, block_200_no_punc)
     file.close()
     
 def main():
@@ -62,7 +75,7 @@ def main():
                 writefile = file[:-3]+'inter'
                 get_block(''.join(filepath)+'/'+file,
                           ''.join(filepath)+'/'+writefile)
-    print('All done!')
+    print("All Done!")
 
 if __name__ == '__main__':
     main()
