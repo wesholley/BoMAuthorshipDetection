891a4933bc9b3e387df841903094974c501fbf49
Christopher Morgan <junk.not@me.com>
2013-11-23 11:35:45 -0700

    Added template to generate our database file. Stubbing the parser for a author-block#.txt files.

---
+++ TestDirectory/bomArffGenerator.py
@@ -0,0 +1,32 @@
+
+
+############################################################
+# This function will return an array that represents the
+# attribute value of %ProperNouns verus Prounouns.
+# the ith value represents the ith instance in our database.
+############################################################
+def generate_array_of_Percent_ProperNouns_Vs_Prounous(data):
+	attribute_array = []
+	return attribute_array
+
+attribute_builder_key = [generate_array_of_Percent_ProperNouns_Vs_Prounous]
+
+############################################################
+# This function will write the bom_data to a weka file_name.arff
+# file.
+############################################################
+def write_data_to_weka_data_file(data, file_name):
+
+	return
+
+
+############################################################
+############################################################
+def write_header_to_file(file_name):
+	return
+
+############################################################
+############################################################
+def write_data_to_file(data, file_name):
+	return
+
--- TestDirectory/posCounter.py
+++ TestDirectory/posCounter.py
@@ -1,14 +1,63 @@
 
 import os
 
+
+
+
+############################################################
+# Example:
+# { 'Nephi': 
+#			[
+#				#block 0
+#				{'NN': [10, ['nephi', 'bob', 'noun'...]],
+#				 'AJ': [20, ['quikly', 'sharply', 'adjictive'...]],
+#				 ...},
+#				 #block 1 
+#				 {'NN': [1,['obo']], ... },
+#				 #block n
+#				 ...
+#			]
+#	'Alma': [...],
+#	...
+#	'Nehpi-Interjections': [ 10, ['o', 'behold', 'interjection', ...]],
+#	'Alma-Interjections': [...],
+# 	...
+# }
+############################################################
 data = {}
 
 
+
+
 ############################################################
-# This function will open a
+# This function will open a file(An author's n^th block of text)
+# and add it to the authors current list of blocks.
+# It will also add all of the parts of speach(pos) the author used
+# in that block and count them as well. 
 ############################################################
 def process_file(fileName):
-	author, block, junk = fileName
+	author, block, junk = fileName.split('-')
+	
+	if not author in data.keys():
+		data[author] = []
+
+	#Generate Block from File
+	block_file = open(fileName, 'r')
+	block = {}
+	for word_and_pos in block_file:
+		word, pos = word_and_pos.strip().split('/')
+
+		#Add the Part of Speech if not already seen
+		if not pos in block.keys():
+			block[pos] = []
+			block[pos].append(0)
+			block[pos].append([])
+
+		block[pos][0] = block[pos][0] + 1 	#Count the number of Parts of Speach
+		block[pos][1].append(word)			#Keep track of each Part of Speach
+
+	#Add Block to Author
+	data[author].append(block)
 
 	return
 
@@ -19,4 +68,7 @@ def process_file(fileName):
 if __name__ == '__main__':
 	for fileName in os.listdir("."):
 	    if fileName.endswith(".txt"):
-	        print fileName
+	        process_file(fileName)
+	        break
+
+	print data
